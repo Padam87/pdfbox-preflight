387a8c2526214124d054521cfc10889c440787c9
Adam Prager <prager.adam87@gmail.com>
2017-09-20 21:51:48 +0200

    NoActions rule fix - annotation link detection

--- src/main/java/com/printmagus/preflight/rule/NoActions.java
+++ src/main/java/com/printmagus/preflight/rule/NoActions.java
@@ -1,17 +1,18 @@
 package com.printmagus.preflight.rule;
 
 import com.printmagus.preflight.Violation;
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.cos.COSObject;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.interactive.action.PDAction;
 import org.apache.pdfbox.pdmodel.interactive.action.PDActionFactory;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * A PDF/X-3 file must not contain Actions or JavaScript.
@@ -42,30 +43,68 @@ public class NoActions extends AbstractRule
         "GoTo3DView"
     );
 
+    PDDocument document;
+    List<Violation> violations;
+
     protected void doValidate(PDDocument document, List<Violation> violations)
     {
+        this.document = document;
+        this.violations = violations;
+
+        for (PDPage page: document.getPages()) {
+            try {
+                for (PDAnnotation annotation: page.getAnnotations()) {
+                    if (annotation instanceof PDAnnotationLink) {
+                        PDAnnotationLink link = (PDAnnotationLink) annotation;
+
+                        addViolation(link.getAction(), page);
+                    }
+                }
+            } catch (IOException e) {
+                // ignore
+            }
+        }
+
         for (COSObject cos: document.getDocument().getObjects()) {
             if (cos.getObject() instanceof COSDictionary) {
                 COSDictionary dictionary = (COSDictionary) cos.getObject();
-                if (dictionary.containsKey(COSName.S)) {
-                    COSName name = (COSName) dictionary.getDictionaryObject(COSName.S);
 
-                    if (ACTIONS.contains(name.getName())) {
-                        HashMap<String, Object> context = new HashMap<String, Object>();
-
-                        context.put("action", PDActionFactory.createAction(dictionary));
-
-                        Violation violation = new Violation(
-                            this.getClass().getSimpleName(),
-                            "Actions and JavaScript prohibited.",
-                            null,
-                            context
-                        );
-
-                        violations.add(violation);
-                    }
+                if (isActionDictionary(dictionary)) {
+                    addViolation(PDActionFactory.createAction(dictionary), null);
                 }
             }
         }
+
+        this.document = null;
+        this.violations = null;
+    }
+
+    private Boolean isActionDictionary(COSDictionary dictionary)
+    {
+        if (dictionary.containsKey(COSName.S)) {
+            COSName name = (COSName) dictionary.getDictionaryObject(COSName.S);
+
+            if (ACTIONS.contains(name.getName())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void addViolation(PDAction action, PDPage page)
+    {
+        HashMap<String, Object> context = new HashMap<String, Object>();
+
+        context.put("action", action);
+
+        Violation violation = new Violation(
+            this.getClass().getSimpleName(),
+            "Actions and JavaScript prohibited.",
+            page != null ? document.getPages().indexOf(page) : null,
+            context
+        );
+
+        violations.add(violation);
     }
 }
